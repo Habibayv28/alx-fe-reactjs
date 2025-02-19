02121e4d1425dd80bad7cd942158a4dfadf6f891
Habiba Yvonne <habibayvonne198@gmail.com>
2025-02-19 22:25:55 +0300

    other components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile name="Habibi" age="35" bio="Loves hiking and photography" /> 
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> 
     </>
   )
 }
