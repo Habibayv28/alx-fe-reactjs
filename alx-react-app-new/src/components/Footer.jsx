c40c3bdf2cefbc1f884598208d9a7e30e6c54c96
Habiba Yvonne <habibayvonne198@gmail.com>
2025-02-20 17:48:29 +0300

    alx-react-app-new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: 'navy', color: 'white', padding: '10px', textAlign: 'center', position: 'fixed', width: '100%', bottom: '0' }}>
+            <p style={{ margin: '0', fontSize: '1em' }}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', backgroundColor: '#f0f0f0', textAlign: 'center', borderRadius: '8px', margin: '20px' }}>
+            <p  style={{ fontSize: '1.2em', color: '#333', lineHeight: '1.6' }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
