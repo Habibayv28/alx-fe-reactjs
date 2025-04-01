ddfe4090c67b69f64e5d30a3c92ac4706a6b795f
Habiba Yvonne <habibayvonne198@gmail.com>
2025-04-01 16:33:52 +0300

    fetching user data

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const BASE_URL = 'https://api.github.com';
-export const fetchGitHubUser = async (username) => {
+export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(`${BASE_URL}/users/${username}`);
     return response.data;
