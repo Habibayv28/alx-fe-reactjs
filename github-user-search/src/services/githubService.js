430d285666401539f0b0a8000d031a1462ca6073
Habiba Yvonne <habibayvonne198@gmail.com>
2025-04-01 17:56:35 +0300

    rectifying my API

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
-const BASE_URL = 'https://api.github.com';
-export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`${BASE_URL}/users/${username}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching GitHub user:', error);
-    throw error;
-  }
+
+const API_URL = import.meta.env.VITE_GITHUB_API_URL || "https://api.github.com/search/users?q";
+
+export const fetchUserData= async ({ username, location, minRepos, page = 1 }) => {
+    const query = [
+        username ? `user:${username}` : '',
+        location ? `location:${location}` : '',
+        minRepos ? `repos:>${minRepos}` : '',
+    ]
+        .filter(Boolean)
+        .join(' ');
+
+    try {
+        const response = await axios.get(`${API_URL}/search/users`, {
+            params: { q: query, page },
+        });
+        return response.data;
+    } catch (error) {
+        throw new Error('Failed to fetch users');
+    }
 };
